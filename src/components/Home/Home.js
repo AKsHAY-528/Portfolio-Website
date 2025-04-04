463d3be62048f236e99d77eedf4610f07036a40f
AKsHAY-528 <trakshaysharma@gmail.com>
2025-04-04 18:02:13 +0530

    home.css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { FaJava } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { FaLaptopCode } from "react-icons/fa";
+import { FaBrain } from "react-icons/fa";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
-import Home2 from "./Home2";
-import Type from "./Type";
 
 function Home() {
   return (
@@ -22,12 +24,8 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name">  AKSHAY SHARMA TR</strong>
+                <strong className="main-name"> AKSHAY SHARMA TR</strong>
               </h1>
-
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
@@ -39,9 +37,36 @@ function Home() {
               />
             </Col>
           </Row>
+
+          {/* Job Role Cards */}
+          <Row className="job-role-container">
+            <Col md={3}>
+              <div className="job-role-card">
+                <FaJava className="job-icon" color="#FF8000" />
+                <span className="job-title">Java Developer</span>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div className="job-role-card">
+                <SiJavascript className="job-icon" color="#FFD700" />
+                <span className="job-title">Full Stack Web Developer</span>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div className="job-role-card">
+                <FaLaptopCode className="job-icon" color="#9B59B6" />
+                <span className="job-title">Software Engineer</span>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div className="job-role-card">
+                <FaBrain className="job-icon" color="#3498DB" />
+                <span className="job-title">AI/ML Enthusiast</span>
+              </div>
+            </Col>
+          </Row>
         </Container>
       </Container>
-      <Home2 />
     </section>
   );
 }
