14b2656a1159408a6cfb3be1d5855772b29af618
AKsHAY-528 <trakshaysharma@gmail.com>
2025-04-04 17:42:03 +0530

    aboutcard

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,12 +8,20 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Akshay Sharma TR </span>
-            from <span className="purple"> Karnataka , India.</span>
+            Hi Everyone, I am <span className="purple"> Akshay Sharma TR </span>
+            from <span className="purple">Karnataka, India.</span>
             <br />
-            I am currently a Final Year CSE Student 
+            I am a final-year Student ,  <span className="purple">Java Developer</span> passionate about building scalable software solutions.
             <br />
-            
+            <br />
+            I have a strong foundation in <span className="purple">Java, Python,</span> and full-stack development,
+            with hands-on experience in <span className="purple">machine learning, web development,</span> and problem-solving.
+            <br />
+            <br />
+            Proven ability to develop high-accuracy models, optimize system performance, and collaborate in fast-paced environments.
+            <br />
+            <br />
+            Currently seeking an entry-level <span className="purple">software development</span> role to contribute technical expertise and drive innovation.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -23,7 +31,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Watching Gmaes
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -31,9 +39,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Code with purpose, learn with passion, and build to inspire."
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Akshay</footer>
         </blockquote>
       </Card.Body>
     </Card>
